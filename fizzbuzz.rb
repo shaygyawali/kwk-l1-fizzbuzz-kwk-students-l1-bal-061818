410c66fbd0e73e01f432f62aad8fc805edd61868
shaygyawali <shaygyawali@gmail.com>
2018-06-19 17:21:49 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,17 +1,20 @@
 
 def fbuzz(x)
-  if (x % 3) && (x % 5) == 0 
+  if x % 15 == 0 
     puts "FizzBuzz"
-    
+  
   elsif x % 3 == 0
     puts "Fizz"
   
   elsif x % 5 == 0
     puts "Buzz"
- 
+  
   else
     puts x
   end
 end
 
+fbuzz(3)
+fbuzz(5)
+fbuzz(15)
 fbuzz(100)
